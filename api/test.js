8f0f711c8516ca9141af641936e9ab3f9c61a725
Ritesh Reddy <riteshpamuru@gmail.com>
2025-09-21 17:49:48 +0530

    chore: update test message to trigger redeploy

--- api/test.js
+++ api/test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = async (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    res.status(200).json({ message: "API and DB are working!" });
+    res.status(200).json({ message: "API and DB are working! v2" });
   } catch (error) {
     console.error('DB connect error:', error);
     res.status(500).json({ message: "DB connection failed", error: error.message });
